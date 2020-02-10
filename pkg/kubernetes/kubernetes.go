9e47ebff1d265dfab4ca16f83db426c331235e3d
Lorenzo Fontana <lo@linux.com>
2020-02-10 17:40:08 +0100

    new(pkg/kubernetes): kubernetes client utilities
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- pkg/kubernetes/kubernetes.go
+++ pkg/kubernetes/kubernetes.go
@@ -1,1 +1,27 @@
 package kubernetes
+
+import (
+	"fmt"
+
+	kube "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func NewKubernetesClientFromConfigPath(configPath string) (*kube.Clientset, error) {
+	// use the current context in kubeconfig
+	if configPath == "" {
+		return nil, fmt.Errorf("kubeconfig path not specified")
+	}
+
+	config, err := clientcmd.BuildConfigFromFlags("", configPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// create the clientset
+	clientset, err := kube.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return clientset, nil
+}
