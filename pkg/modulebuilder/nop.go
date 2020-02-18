a507c2150150534bac05561ac3bf4b8e03980da3
Lorenzo Fontana <lo@linux.com>
2020-02-19 00:29:47 +0100

    new(pkg/buildprocessor): use modulestorage in no-op buildprocessor
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- pkg/modulebuilder/nop.go
+++ pkg/modulebuilder/nop.go
@@ -3,12 +3,15 @@ package modulebuilder
 import (
 	"context"
 
+	"github.com/falcosecurity/build-service/pkg/filesystem"
+	"github.com/falcosecurity/build-service/pkg/modulebuilder/build"
 	"go.uber.org/zap"
 )
 
 type NopBuildProcessor struct {
-	ctx    context.Context
-	logger *zap.Logger
+	ctx           context.Context
+	logger        *zap.Logger
+	modulestorage *filesystem.ModuleStorage
 }
 
 func NewNopBuildProcessor() *NopBuildProcessor {
@@ -23,11 +26,15 @@ func (bp *NopBuildProcessor) WithLogger(logger *zap.Logger) {
 	bp.logger = logger
 }
 
+func (bp *NopBuildProcessor) WithModuleStorage(ms *filesystem.ModuleStorage) {
+	bp.modulestorage = ms
+}
+
 func (bp *NopBuildProcessor) String() string {
 	return "no-op"
 }
 
-func (bp *NopBuildProcessor) Request(b Build) error {
+func (bp *NopBuildProcessor) Request(b build.Build) error {
 	// just ignore everything
 	return nil
 }
