bf1d26005c876612853fd0039216dd18eef2ea40
Lorenzo Fontana <lo@linux.com>
2020-02-27 11:11:38 +0100

    new: nop doesn't really need to do anything
    Co-Authored-By: Leonardo Di Donato <leodidonato@gmail.com>
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- pkg/modulebuilder/nop.go
+++ pkg/modulebuilder/nop.go
@@ -1,53 +1,24 @@
 package modulebuilder
 
 import (
-	"context"
-
-	"github.com/falcosecurity/build-service/pkg/filesystem"
 	"github.com/falcosecurity/build-service/pkg/modulebuilder/build"
 	"go.uber.org/zap"
 )
 
 type NopBuildProcessor struct {
-	ctx           context.Context
-	logger        *zap.Logger
-	modulestorage *filesystem.ModuleStorage
 }
 
 func NewNopBuildProcessor() *NopBuildProcessor {
 	return &NopBuildProcessor{}
 }
 
-func (bp *NopBuildProcessor) WithContext(c context.Context) {
-	bp.ctx = c
-}
-
 func (bp *NopBuildProcessor) WithLogger(logger *zap.Logger) {
-	bp.logger = logger
-}
-
-func (bp *NopBuildProcessor) WithModuleStorage(ms *filesystem.ModuleStorage) {
-	bp.modulestorage = ms
 }
 
 func (bp *NopBuildProcessor) String() string {
 	return "no-op"
 }
 
-func (bp *NopBuildProcessor) Request(b build.Build) error {
-	// just ignore everything
+func (bp *NopBuildProcessor) Start(b build.Build) error {
 	return nil
-}
-
-func (bp *NopBuildProcessor) Start() error {
-	// I don't do anything and just sit here pretending I'm working
-	// but I'm Nop so that's what I do!
-	for {
-		select {
-		case <-bp.ctx.Done():
-			return nil
-		default:
-			continue
-		}
-	}
 }
