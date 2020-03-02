3bcc892d92c427ad4831436254c38d38da7fe90b
Leonardo Di Donato <leodidonato@gmail.com>
2020-03-02 23:38:37 +0000

    new(cmd): initial validators for root options
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

--- cmd/root_options.go
+++ cmd/root_options.go
@@ -4,17 +4,17 @@ import (
 	"log"
 
 	"github.com/creasty/defaults"
+	"github.com/falcosecurity/driverkit/validate"
 )
 
 // RootOptions ...
 type RootOptions struct {
-	ConfigFile       string `default:".driverkit.yaml" validate:"file"`
-	Output           string
-	ModuleVersion    string `default:"dev"`
-	KernelVersion    string
-	KernelRelease    string
-	Target           string
-	KernelConfigData string
+	Output           string `validate:"required,file"`
+	ModuleVersion    string `default:"dev" validate:"required,ascii"` // todo > semver validator?
+	KernelVersion    string `validate:"required,number"`              // todo > semver validator?
+	KernelRelease    string `validate:"required,ascii"`
+	Target           string `validate:"oneof=vanilla ubuntu-generic ubuntu-aws"`
+	KernelConfigData string `validate:"base64"` // todo > tie to target field - e.g, `required_without_ubuntu`
 }
 
 // NewRootOptions ...
@@ -23,5 +23,11 @@ func NewRootOptions() *RootOptions {
 	if err := defaults.Set(rootOpts); err != nil {
 		log.Fatal(err)
 	}
+	// V.RegisterStructValidation(RootOptionsLevelValidation, RootOptions{})
 	return rootOpts
+}
+
+// Validate validates the RootOptions fields.
+func (ro *RootOptions) Validate() error {
+	return validate.V.Struct(ro)
 }
