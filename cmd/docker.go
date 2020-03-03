24b9c79211dad69a3e92419b555ef39dae33c0b5
Lorenzo Fontana <lo@linux.com>
2020-03-03 16:04:59 +0100

    update(cmd): using the new processor interface in commands
    Co-Authored-By: Leonardo Di Donato <leodidonato@gmail.com>
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- cmd/docker.go
+++ cmd/docker.go
@@ -4,27 +4,19 @@ import (
 	"fmt"
 
 	"github.com/falcosecurity/driverkit/pkg/modulebuilder"
-	"github.com/falcosecurity/driverkit/pkg/modulebuilder/build"
-	"github.com/falcosecurity/driverkit/pkg/modulebuilder/buildtype"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
 )
 
 // NewDockerCmd ...
-func NewDockerCmd() *cobra.Command {
+func NewDockerCmd(rootOpts *RootOptions) *cobra.Command {
 	dockerCmd := &cobra.Command{
 		Use:   "docker",
 		Short: "run driverkit against a docker daemon",
 		Long:  "This is the actual command to use a docker daemon with driverkit",
 		RunE: func(c *cobra.Command, args []string) error {
-			b, err := a(c.Flags())
-			if err != nil {
-				return err
-			}
-			// spew.Dump(b)
-
+			b := rootOpts.toBuild()
 			processor := modulebuilder.NewDockerBuildProcessor()
-			return processor.Start(*b)
+			return processor.Start(b)
 		},
 	}
 
@@ -34,44 +26,4 @@ func NewDockerCmd() *cobra.Command {
 	})
 
 	return dockerCmd
-}
-
-func a(pf *pflag.FlagSet) (*build.Build, error) {
-	outputFileName, err := pf.GetString("output")
-	if err != nil {
-		return nil, err
-	}
-	moduleVersion, err := pf.GetString("moduleversion")
-	if err != nil {
-		return nil, err
-	}
-	kernelVersion, err := pf.GetString("kernelversion")
-	if err != nil {
-		return nil, err
-	}
-	kernelRelease, err := pf.GetString("kernelrelease")
-	if err != nil {
-		return nil, err
-	}
-	buildType, err := pf.GetString("target")
-	if err != nil {
-		return nil, err
-	}
-	kernelConfigData, err := pf.GetString("kernelconfigdata")
-	if err != nil {
-		return nil, err
-	}
-	if len(kernelConfigData) == 0 {
-		kernelConfigData = "bm8tZGF0YQ==" // no-data
-	}
-
-	return &build.Build{
-		ModuleVersion:    moduleVersion,
-		KernelVersion:    kernelVersion,
-		KernelRelease:    kernelRelease,
-		Architecture:     string(modulebuilder.BuildArchitectureX86_64), // TODO(fntlnz,leodido): make this configurable
-		BuildType:        buildtype.BuildType(buildType),
-		KernelConfigData: kernelConfigData,
-		OutputFilePath:   outputFileName,
-	}, nil
 }
