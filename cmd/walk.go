28f883c78c1fdc3933d770aea9af7f4ddfe7683f
Leonardo Di Donato <leodidonato@gmail.com>
2020-03-04 13:59:35 +0000

    update(cmd): patch all the help messages for all commands
    Co-authored-by: Lorenzo Fontana <lo@linux.com>
    Signed-off-by: Leonardo Di Donato <leodidonato@gmail.com>

---
+++ cmd/walk.go
@@ -0,0 +1,11 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+// walk calls f for c and all of its children.
+func walk(c *cobra.Command, f func(*cobra.Command)) {
+	f(c)
+	for _, c := range c.Commands() {
+		walk(c, f)
+	}
+}
