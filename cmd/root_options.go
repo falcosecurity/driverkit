9622f8b3b3105c9dd2460c54200a8f524b08a49d
Lorenzo Fontana <lo@linux.com>
2020-03-06 17:13:35 +0100

    new(pkg/modulebuilder): target flag supports debian too
    Co-Authored-By: Leonardo Di Donato <leodidonato@gmail.com>
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- cmd/root_options.go
+++ cmd/root_options.go
@@ -19,7 +19,7 @@ type RootOptions struct {
 	ModuleVersion    string `default:"dev" validate:"required,eq=dev|sha1" name:"module version"` // todo > semver validator?
 	KernelVersion    uint16 `validate:"omitempty,number" name:"kernel version"`                   // todo > semver validator?
 	KernelRelease    string `validate:"required,ascii" name:"kernel release"`
-	Target           string `validate:"required,oneof=vanilla ubuntu-generic ubuntu-aws centos" name:"target"`
+	Target           string `validate:"required,oneof=vanilla ubuntu-generic ubuntu-aws centos debian" name:"target"`
 	KernelConfigData string `validate:"omitempty,base64" name:"kernel config data"` // fixme > tag "name" does not seem to work when used at struct level, but works when used at inner level
 }
 
