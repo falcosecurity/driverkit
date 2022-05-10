f4cc16e9b6b9a08a17ebb110f202dee89b2bbfd7
Luca Guerra <luca@guerra.sh>
2022-05-10 16:00:10 +0200

    update(cmd): do not print the default option in the args
    Signed-off-by: Luca Guerra <luca@guerra.sh>

--- cmd/root.go
+++ cmd/root.go
@@ -7,7 +7,6 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/falcosecurity/driverkit/pkg/driverbuilder"
 	"github.com/falcosecurity/driverkit/pkg/driverbuilder/builder"
 	"github.com/falcosecurity/driverkit/pkg/version"
 	"github.com/spf13/cobra"
@@ -118,7 +117,7 @@ func NewRootCmd() *RootCmd {
 	flags.StringVar(&rootOpts.KernelConfigData, "kernelconfigdata", rootOpts.KernelConfigData, "base64 encoded kernel config data: in some systems it can be found under the /boot directory, in other it is gzip compressed under /proc")
 	flags.StringVar(&rootOpts.ModuleDeviceName, "moduledevicename", rootOpts.ModuleDeviceName, "kernel module device name (the default is falco, so the device will be under /dev/falco*)")
 	flags.StringVar(&rootOpts.ModuleDriverName, "moduledrivername", rootOpts.ModuleDriverName, "kernel module driver name, i.e. the name you see when you check installed modules via lsmod")
-	flags.StringVar(&rootOpts.BuilderImage, "builderimage", rootOpts.BuilderImage, fmt.Sprintf("docker image to be used to build the kernel module and eBPF probe. If not provided, the default image will be used. (default \"%v\")", driverbuilder.BuilderBaseImage))
+	flags.StringVar(&rootOpts.BuilderImage, "builderimage", rootOpts.BuilderImage, "docker image to be used to build the kernel module and eBPF probe. If not provided, the default image will be used.")
 	viper.BindPFlags(flags)
 
 	// Flag annotations and custom completions
