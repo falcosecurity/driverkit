5efb0e49f709516fc21301e114bbb0310c267555
Federico Di Pierro <nierro92@gmail.com>
2022-06-24 17:01:41 +0200

    chore(pkg): ported redhat builder to use go:embed.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- pkg/driverbuilder/builder/redhat.go
+++ pkg/driverbuilder/builder/redhat.go
@@ -2,9 +2,13 @@ package builder
 
 import (
 	"bytes"
+	_ "embed"
 	"github.com/falcosecurity/driverkit/pkg/kernelrelease"
 	"text/template"
 )
+
+//go:embed templates/redhat.sh
+var redhatTemplate string
 
 // TargetTypeRedhat identifies the redhat target.
 const TargetTypeRedhat Type = "redhat"
@@ -27,57 +31,13 @@ type redhatTemplateData struct {
 	BuildProbe        bool
 }
 
-const redhatTemplate = `
-#!/bin/bash
-set -xeuo pipefail
-
-rm -Rf {{ .DriverBuildDir }}
-mkdir {{ .DriverBuildDir }}
-rm -Rf /tmp/module-download
-mkdir -p /tmp/module-download
-
-curl --silent -SL {{ .ModuleDownloadURL }} | tar -xzf - -C /tmp/module-download
-mv /tmp/module-download/*/driver/* {{ .DriverBuildDir }}
-
-cp /driverkit/module-Makefile {{ .DriverBuildDir }}/Makefile
-bash /driverkit/fill-driver-config.sh {{ .DriverBuildDir }}
-
-# Fetch the kernel
-rm -Rf /tmp/kernel-download
-mkdir /tmp/kernel-download
-cd /tmp/kernel-download
-yum install -y --downloadonly --downloaddir=/tmp/kernel-download kernel-devel-0:{{ .KernelPackage }}
-rpm2cpio kernel-devel-{{ .KernelPackage }}.rpm | cpio --extract --make-directories
-
-rm -Rf /tmp/kernel
-mkdir -p /tmp/kernel
-mv usr/src/kernels/*/* /tmp/kernel
-
-{{ if .BuildModule }}
-# Build the module
-cd {{ .DriverBuildDir }}
-make KERNELDIR=/tmp/kernel
-mv {{ .ModuleDriverName }}.ko {{ .ModuleFullPath }}
-strip -g {{ .ModuleFullPath }}
-# Print results
-modinfo {{ .ModuleFullPath }}
-{{ end }}
-
-{{ if .BuildProbe }}
-# Build the eBPF probe
-cd {{ .DriverBuildDir }}/bpf
-make LLC=/usr/bin/llc CLANG=/usr/bin/clang CC=/usr/bin/gcc KERNELDIR=/tmp/kernel
-ls -l probe.o
-{{ end }}
-`
-
 func (v redhat) Script(cfg Config, kr kernelrelease.KernelRelease) (string, error) {
 	t := template.New(string(TargetTypeRedhat))
 	parsed, err := t.Parse(redhatTemplate)
 	if err != nil {
 		return "", err
 	}
-	
+
 	td := redhatTemplateData{
 		DriverBuildDir:    DriverDirectory,
 		KernelPackage:     kr.Fullversion + kr.FullExtraversion,
