a5394ccdbe91519afaa4f6a544cad8d25b4789ac
Federico Di Pierro <nierro92@gmail.com>
2022-08-29 09:59:26 +0200

    chore(docs,pkg): moved builder.README under the docs subfolder with some enhanchements.
    Moreover, added a new doc file to explaing builder images and how to add new ones.
    Finally, added some `defaultGCC` unit tests.
    
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

---
+++ pkg/driverbuilder/builder/builders_test.go
@@ -0,0 +1,74 @@
+package builder
+
+import (
+	"github.com/falcosecurity/driverkit/pkg/kernelrelease"
+	"testing"
+)
+
+var gccTests = []struct {
+	config      kernelrelease.KernelRelease
+	expectedGCC float64
+}{
+	{
+		config: kernelrelease.KernelRelease{
+			Fullversion:      "4.15.0",
+			Version:          4,
+			PatchLevel:       15,
+			Sublevel:         0,
+			Extraversion:     "188",
+			FullExtraversion: "-188",
+			Architecture:     "amd64",
+		},
+		expectedGCC: 8,
+	},
+	{
+		config: kernelrelease.KernelRelease{
+			Fullversion:      "5.15.0",
+			Version:          5,
+			PatchLevel:       15,
+			Sublevel:         0,
+			Extraversion:     "1004-intel-iotg",
+			FullExtraversion: "-1004-intel-iotg",
+			Architecture:     "amd64",
+		},
+		expectedGCC: 11,
+	},
+	{
+		config: kernelrelease.KernelRelease{
+			Fullversion:      "3.13.0",
+			Version:          3,
+			PatchLevel:       13,
+			Sublevel:         0,
+			Extraversion:     "100",
+			FullExtraversion: "-100",
+			Architecture:     "amd64",
+		},
+		expectedGCC: 5,
+	},
+	{
+		config: kernelrelease.KernelRelease{
+			Fullversion:      "5.18.0",
+			Version:          5,
+			PatchLevel:       18,
+			Sublevel:         0,
+			Extraversion:     "1001-kvm",
+			FullExtraversion: "-1001-kvm",
+			Architecture:     "amd64",
+		},
+		expectedGCC: 12,
+	},
+}
+
+func TestDefaultGCC(t *testing.T) {
+	for _, test := range gccTests {
+		// call function
+		selectedGCC := defaultGCC(test.config)
+
+		// compare errors
+		// there are no official errors, so comparing fmt.Errorf() doesn't really work
+		// compare error message text instead
+		if test.expectedGCC != selectedGCC {
+			t.Fatalf("SelectedGCC (%f) != expectedGCC (%f) with kernelrelease: '%v'", selectedGCC, test.expectedGCC, test.config)
+		}
+	}
+}
