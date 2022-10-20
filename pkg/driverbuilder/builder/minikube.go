d52c9dfe31d0775da955e605dda75a7c43ba1f4c
Aldo Lacuku <aldo@lacuku.eu>
2022-10-20 10:16:49 +0200

    update(builder/minikube): implement the GCCVersionRequestor interface for the minikube builder
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- pkg/driverbuilder/builder/minikube.go
+++ pkg/driverbuilder/builder/minikube.go
@@ -1,8 +1,43 @@
 package builder
 
+import (
+	"github.com/blang/semver"
+	"github.com/falcosecurity/driverkit/pkg/kernelrelease"
+)
+
 // TargetTypeMinikube identifies the Minikube target.
 const TargetTypeMinikube Type = "minikube"
 
 func init() {
-	BuilderByTarget[TargetTypeMinikube] = &vanilla{}
+	BuilderByTarget[TargetTypeMinikube] = &minikube{
+		vanilla{},
+	}
+}
+
+type minikube struct {
+	vanilla
+}
+
+func (m *minikube) Name() string {
+	return TargetTypeMinikube.String()
+}
+
+func (m *minikube) TemplateData(c Config, kr kernelrelease.KernelRelease, urls []string) interface{} {
+	return vanillaTemplateData{
+		commonTemplateData: c.toTemplateData(m, kr),
+		KernelDownloadURL:  urls[0],
+		KernelLocalVersion: kr.FullExtraversion,
+	}
+}
+
+func (m *minikube) GCCVersion(kr kernelrelease.KernelRelease) semver.Version {
+	// The supported versions of minikube use kernels > 4.19.
+	switch kr.Major {
+	case 5:
+		return semver.Version{Major: 10}
+	case 4:
+		return semver.Version{Major: 8}
+	default:
+		return semver.Version{Major: 12}
+	}
 }
