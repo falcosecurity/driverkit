8795345094709ac13a61daf52a98a3f8cdc67370
Logan Bond <logantbond@gmail.com>
2023-03-22 18:34:13 -0500

    adding in initial oracle support
    Signed-off-by: Logan Bond <logantbond@gmail.com>
    
    remove uek from non-uek kernels
    
    Signed-off-by: Logan Bond <logantbond@gmail.com>
    
    add oracle 8 url
    
    Signed-off-by: Logan Bond <logantbond@gmail.com>
    
    make comments more explicit
    
    Signed-off-by: Logan Bond <logantbond@gmail.com>
    
    add an exception that needs handled for Oracle 8 versioning
    
    Signed-off-by: Logan Bond <logantbond@gmail.com>
    
    make comment more explicit
    
    Signed-off-by: Logan Bond <logantbond@gmail.com>
    
    add gawk to build image, seems useful
    
    Signed-off-by: Logan Bond <lbond@secureworks.com>
    
    uncomment gcc 4.9
    
    Signed-off-by: Logan Bond <lbond@secureworks.com>

---
+++ pkg/driverbuilder/builder/templates/oracle.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -xeuo pipefail
+
+rm -Rf {{ .DriverBuildDir }}
+mkdir {{ .DriverBuildDir }}
+rm -Rf /tmp/module-download
+mkdir -p /tmp/module-download
+
+curl --silent -SL {{ .ModuleDownloadURL }} | tar -xzf - -C /tmp/module-download
+mv /tmp/module-download/*/driver/* {{ .DriverBuildDir }}
+
+cp /driverkit/module-Makefile {{ .DriverBuildDir }}/Makefile
+bash /driverkit/fill-driver-config.sh {{ .DriverBuildDir }}
+
+# Fetch the kernel
+mkdir /tmp/kernel-download
+cd /tmp/kernel-download
+curl --silent -o kernel-devel.rpm -SL {{ .KernelDownloadURL }}
+rpm2cpio kernel-devel.rpm | cpio --extract --make-directories
+rm -Rf /tmp/kernel
+mkdir -p /tmp/kernel
+mv usr/src/kernels/*/* /tmp/kernel
+
+{{ if .BuildModule }}
+# Build the module
+cd {{ .DriverBuildDir }}
+make CC=/usr/bin/gcc-{{ .GCCVersion }} KERNELDIR=/tmp/kernel
+mv {{ .ModuleDriverName }}.ko {{ .ModuleFullPath }}
+strip -g {{ .ModuleFullPath }}
+# Print results
+modinfo {{ .ModuleFullPath }}
+{{ end }}
+
+{{ if .BuildProbe }}
+# Build the eBPF probe
+cd {{ .DriverBuildDir }}/bpf
+make KERNELDIR=/tmp/kernel
+ls -l probe.o
+{{ end }}
