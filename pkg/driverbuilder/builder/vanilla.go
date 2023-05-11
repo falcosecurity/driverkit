829757e9faafd652a62a6404a66015eb488ffcd8
Federico Di Pierro <nierro92@gmail.com>
2023-05-11 17:46:45 +0200

    new(pkg): support RC releases for vanilla target.
    Signed-off-by: Federico Di Pierro <nierro92@gmail.com>

--- pkg/driverbuilder/builder/vanilla.go
+++ pkg/driverbuilder/builder/vanilla.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"fmt"
 	"github.com/falcosecurity/driverkit/pkg/kernelrelease"
+	"strings"
 )
 
 //go:embed templates/vanilla.sh
@@ -24,6 +25,7 @@ type vanillaTemplateData struct {
 	commonTemplateData
 	KernelDownloadURL  string
 	KernelLocalVersion string
+	IsTarGz            bool
 }
 
 func (v *vanilla) Name() string {
@@ -43,9 +45,20 @@ func (v *vanilla) TemplateData(c Config, kr kernelrelease.KernelRelease, urls []
 		commonTemplateData: c.toTemplateData(v, kr),
 		KernelDownloadURL:  urls[0],
 		KernelLocalVersion: kr.FullExtraversion,
+		IsTarGz:            strings.HasSuffix(urls[0], ".tar.gz"), // Since RC have a tar.gz format, we need to inform the build script
 	}
 }
 
 func fetchVanillaKernelURLFromKernelVersion(kv kernelrelease.KernelRelease) string {
+	// Note: even non RC are available as tar.gz; but tar.xz are much smaller
+	// and thus much quicker to download. Let's keep tar.xz for non RC!
+	// Numbers: 110M (tar.gz) vs 75M (tar.xz)
+	if isRC(kv) {
+		return fmt.Sprintf("https://git.kernel.org/torvalds/t/linux-%s%s.tar.gz", kv.Fullversion, kv.FullExtraversion)
+	}
 	return fmt.Sprintf("https://cdn.kernel.org/pub/linux/kernel/v%d.x/linux-%s.tar.xz", kv.Major, kv.Fullversion)
+}
+
+func isRC(kv kernelrelease.KernelRelease) bool {
+	return strings.Contains(kv.Extraversion, "rc")
 }
