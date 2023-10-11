42bb473e58d504d8d65315b92686d71d2f47640c
Leonardo Grasso <me@leonardograsso.com>
2023-10-11 11:05:23 +0200

    docs: add license headers and spdx id
    Signed-off-by: Leonardo Grasso <me@leonardograsso.com>

--- pkg/driverbuilder/docker.go
+++ pkg/driverbuilder/docker.go
@@ -1,3 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+/*
+Copyright (C) 2023 The Falco Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package driverbuilder
 
 import (
@@ -7,6 +21,14 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"log/slog"
+	"os"
+	"runtime"
+	"strconv"
+
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
@@ -15,14 +37,7 @@ import (
 	"github.com/falcosecurity/driverkit/pkg/kernelrelease"
 	"github.com/falcosecurity/driverkit/pkg/signals"
 	v1 "github.com/opencontainers/image-spec/specs-go/v1"
-	"io"
-	"io/ioutil"
 	"k8s.io/apimachinery/pkg/util/uuid"
-	"log"
-	"log/slog"
-	"os"
-	"runtime"
-	"strconv"
 )
 
 // DockerBuildProcessorName is a constant containing the docker name.
